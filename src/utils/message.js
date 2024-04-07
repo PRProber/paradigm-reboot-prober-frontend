7ee1a9ba860a6da2acbcbc99c17101b6e44fddad
IceLocke <lockeice@gmail.com>
2024-04-07 14:42:08 +0800

    添加 i18n 定义

--- src/utils/message.js
+++ src/utils/message.js
@@ -17,14 +17,21 @@ const messages = {
             cover: "Cover",
             illustrator: "Illustrator",
             version: "Version",
+            b35orb15: "New / Old Version",
             b35: "Old version",
             b15: "New version",
+            level: "Level",
+            fitting_level: "Fitting Level",
+            difficulty: "Difficulty",
             album: "Album",
             genre: "Genre",
-            length: "Length"
+            length: "Length",
+            notes: "Notes"
         },
         message: {
-            welcome: "Welcome, {username}! "
+            welcome: "Welcome, {username}! ",
+            success_get: "Successfully get {data_name} data",
+            search: "Type to search by song title"
         }
     },
     zh_cn: {
@@ -38,21 +45,28 @@ const messages = {
             b50: "Best 50",
             records: "游玩记录",
             best_records: "最佳记录",
-            song_levels: "歌曲列表",
+            song_levels: "定数表",
             single_info: "单曲信息",
             title: "曲名",
             artist: "曲师",
             cover: "曲绘",
+            difficulty: "难度",
+            level: "定数",
+            fitting_level: "拟合定数",
             illustrator: "画师",
             version: "版本",
+            b35orb15: "新/旧版本",
             b35: "旧版本",
             b15: "新版本",
             album: "专辑",
             genre: "流派",
-            length: "曲长"
+            length: "曲长",
+            notes: "音符数量"
         },
         message: {
-            welcome: "欢迎，{username}！"
+            welcome: "欢迎，{username}！",
+            success_get: "成功获取 {data_name} 数据",
+            search: "搜素曲名"
         }
     }
 }
