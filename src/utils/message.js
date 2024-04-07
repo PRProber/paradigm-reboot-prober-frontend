268dd134c185b948a0767c061a5663f01dcfa122
IceLocke <lockeice@gmail.com>
2024-04-07 18:45:29 +0800

    添加 i18n

--- src/utils/message.js
+++ src/utils/message.js
@@ -17,10 +17,12 @@ const messages = {
             b50: "Best 50",
             records: "Play Records",
             best_records: "Best Records",
+            song_level_id: "Level ID",
             song_levels: "Song Levels",
             single_info: "Single Info",
             title: "Title",
             artist: "Composer",
+            level_design: "Level Design",
             cover: "Cover",
             illustrator: "Illustrator",
             version: "Version",
@@ -48,7 +50,9 @@ const messages = {
             register_success: "Register success",
             register_failed: "Register failed, details: ",
             login_success: "Login success",
-            login_failed: "Login failed, possibly wrong username or password. details: "
+            login_failed: "Login failed, possibly wrong username or password. details: ",
+            get_my_info_success: "Successfully get user profile",
+            get_my_info_failed: "Failed to get user profile. Details: "
         }
     },
     zh_cn: {
@@ -68,6 +72,7 @@ const messages = {
         term: {
             b50: "Best 50",
             records: "游玩记录",
+            song_level_id: "谱面 ID",
             best_records: "最佳记录",
             song_levels: "定数表",
             single_info: "单曲信息",
@@ -77,6 +82,7 @@ const messages = {
             difficulty: "难度",
             level: "定数",
             fitting_level: "拟合定数",
+            level_design: "谱师",
             illustrator: "画师",
             version: "版本",
             b35orb15: "新/旧版本",
@@ -100,7 +106,9 @@ const messages = {
             register_success: "注册成功",
             register_failed: "注册失败，详情：",
             login_success: "登入成功",
-            login_failed: "登入成功，可能是用户名或者密码错误。详情： "
+            login_failed: "登入成功，可能是用户名或者密码错误。详情： ",
+            get_my_info_success: "成功获取个人资料",
+            get_my_info_failed: "获取个人资料失败，详情："
         }
     }
 }
