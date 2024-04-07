d61c16137638b90e8e74ba7988bc10a3565be942
IceLocke <lockeice@gmail.com>
2024-04-07 14:42:14 +0800

    封装 API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,73 +1,82 @@
 import axios from "axios";
 import { useUserStore } from "@/utils/store";
+import { createPinia } from "pinia";
 
-const userStore = useUserStore()
+const pinia = createPinia()
+const userStore = useUserStore(pinia)
 
-const createAxiosClient = (access_token) => {
-    return axios.create({
-        baseURL: API_BASE,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + access_token
-        }
-    })
+const beforeRequest = config => {
+    const userStorage = JSON.parse(localStorage.getItem('user-store'))
+    userStorage && (config.headers.Authorization = userStorage['access_token'])
+    return config
 }
 
 const API_BASE = 'http://localhost:8000/api/v1';
-let client = createAxiosClient('');
-
-userStore.$subscribe((mutation, state) => {
-    client = createAxiosClient(state.access_token)
+let client = axios.create({
+    baseURL: API_BASE,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:8000',
+        'Access-Control-Allow-Credentials': 'true',
+    },
 })
 
-export default {
-    methods: {
-        // User
-        login(username, password) {
-            return client.post('/user/login', {
-                username: username,
-                password: password,
-            })
-        },
-        register(username, email, password) {
-            return client.post('/user/register', {
-                username: username,
-                email: email,
-                password: password,
-            })
-        },
-        getMyInfo() {
-            return client.get('/user/me')
-        },
+client.interceptors.request.use(beforeRequest)
+userStore.$subscribe((_, state) => {
+    localStorage.setItem('user-store', JSON.stringify({ ...state }))
+})
 
-        // Song
-        getAllSongLevels() {
-            return client.get('/songs')
-        },
-        createSong(formData) {
-            return client.post('/songs', formData)
-        },
-        updateSong(formData) {
-            return client.patch('/songs', formData)
-        },
+export const login = (username, password) => {
+    return client.post('/user/login', {
+        username: username,
+        password: password,
+    })
+}
 
-        // Record
-        getAllRecords(username) {
-            return client.get('/records' + username)
-        },
-        getAllBestRecords(username) {
-            return client.get('/records + username', {
-                params: { best: true }
-            })
-        },
-        getBest50Records(underflow = 0) {
-            return client.get('/user/records', {
-                params: { b50: true, underflow: underflow }
-            })
-        },
-        postRecord(formData) {
-            return client.post('/user/records', formData)
-        }
+export const register = (username, email, password) => {
+    return client.post('/user/register', {
+        username: username,
+        email: email,
+        password: password,
+    })
+}
 
-    }
+export const getMyInfo = () => {
+    return client.get('/user/me')
+}
+
+export const getAllSongLevels = () => {
+    return client.get('/songs')
+}
+
+export const getSingleSongInfo = (song_id) => {
+    return client.get('/songs/' + song_id)
+}
+
+export const createSong = (formData) => {
+    return client.post('/songs', formData)
+}
+
+export const updateSong = (formData) => {
+    return client.patch('/songs', formData)
+}
+
+export const getAllRecords = (username) => {
+    return client.get('/records/' + username)
+}
+
+export const getAllBestRecords = (username) => {
+    return client.get('/records/' + username, {
+        params: { best: true }
+    })
+}
+
+export const getBest50Records = (underflow = 0) => {
+    return client.get('/user/records', {
+        params: {b50: true, underflow: underflow}
+    })
+}
+
+export const postRecord = (formData = 0) => {
+    return client.post('/user/records', formData)
 }
